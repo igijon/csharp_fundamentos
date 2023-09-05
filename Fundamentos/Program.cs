018c65b2b716309986c230052bc7d83919c08d8d
Inma Gijón <igijoninf@gmail.com>
2023-09-05 19:52:55 +0200

    Entrada y salida de datos

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 10, y = 4;
+Console.WriteLine("x: {0} e y: {1}", x, y);
+Console.WriteLine("x: "+ x +" e y: "+ y);
 
+Console.WriteLine("Introduce un valor: ");
+x = int.Parse(Console.ReadLine()); //Conversión explícita que si no recibe un entero fallará
+Console.WriteLine("x: {0}", x);
+
+Console.ReadKey(); //Detenemos la consola hasta que el usuario pulsa una tecla
+
+
+ 
