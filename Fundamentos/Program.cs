ae3313045579aa1f2a161b27f68e0645a8012de6
Inma Gijón <igijoninf@gmail.com>
2023-09-06 12:35:19 +0200

    Paso por valor y por referencia

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,4 +1,6 @@
-﻿int x = 10, y = 4;
+﻿using Fundamentos;
+
+int x = 10, y = 4;
 Console.WriteLine("x: {0} e y: {1}", x, y);
 Console.WriteLine("x: "+ x +" e y: "+ y);
 
@@ -6,7 +8,10 @@ Console.WriteLine("Introduce un valor: ");
 x = int.Parse(Console.ReadLine()); //Conversión explícita que si no recibe un entero fallará
 Console.WriteLine("x: {0}", x);
 
+Console.WriteLine("Antes de Intercambiar por Valor: x: {0} y: {1}", x, y);
+Utilidades.intercambio(x, y);
+Console.WriteLine("Después de Intercambiar por Valor: x: {0} y: {1}", x, y);
+Utilidades.intercanmbioRef(ref x, ref y);
+Console.WriteLine(" de Intercambiar por Referencia: x: {0} y: {1}", x, y);
+
 Console.ReadKey(); //Detenemos la consola hasta que el usuario pulsa una tecla
-
-
- 
---
+++ Fundamentos/Utilidades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Fundamentos
+{
+	public class Utilidades
+	{
+		public static void intercambio(int a, int b)
+		{
+			int c;
+			c = a; a = b; b = c;
+		}
+
+		public static void intercanmbioRef(ref int a, ref int b)
+		{
+            int c;
+            c = a; a = b; b = c;
+        }
+	}
+}
+
